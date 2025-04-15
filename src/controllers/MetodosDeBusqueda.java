53bcb5e0714d74400ada3b9fce95de2dc6a7d08a
Xristianz <moscosocristhian130@gmail.com>
2025-04-15 07:48:16 -0500

    Segundo commit

--- src/controllers/MetodosDeBusqueda.java
+++ src/controllers/MetodosDeBusqueda.java
@@ -1,6 +1,13 @@
 package controllers;
 
 public class MetodosDeBusqueda {
-    
+    public static int busquedaLineal(int[] arreglo, int objetivo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == objetivo) {
+                return i; // Devolver el índice si se encuentra
+            }
+        }
+        return -1; // Devolver -1 si no se encuentra
+    }
     
 }
